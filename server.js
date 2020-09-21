46a561dc3ae5d63e5b1b54948b0908ec6fa23fd6
Danny Lai <dannyphlai@gmail.com>
2020-09-21 16:49:33 +1000

    Added checkAuth function; will be used to lock down certain routes

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const jwtAuthenticate = require('express-jwt');
 
+const checkAuth = () => {
+  return jwtAuthenticate({
+      secret: SERVER_SECRET_KEY,
+      algorithms: ['HS256']
+  });
+};
+
+// TODO: Move into .env or .bash_profile
+const SERVER_SECRET_KEY = 'notVerySecret'
 
 mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true, useUnifiedTopology: true});
 
